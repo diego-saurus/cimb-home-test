e873799968c44496a3571bbd05e1ba9258d20b4e
diego-saurus <aldiegoid20019@gmail.com>
2026-07-31 16:49:12 +0700

    feat: setup nuxt ui as main ui library

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,13 @@
+import './assets/css/main.css'
+import ui from '@nuxt/ui/vue-plugin'
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import './style.css'
+import router from './router'
 
 const app = createApp(App)
+
+app.use(router)
+app.use(ui)
 
 app.mount('#app')
---
+++ frontend/src/pages/MainPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UContainer class="py-12">
+    <HelloWorld />
+  </UContainer>
+</template>
