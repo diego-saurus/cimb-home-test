f6494b3fd4683a08ec68b12cc55be49e5befcff9
diego-saurus <aldiegoid20019@gmail.com>
2026-08-01 01:29:22 +0700

    feat(backend): change from demo to call monitoring application

---
+++ backend/src/test/java/dev/diegosaurus/cimb/callmonitoring/CallMonitoringApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.diegosaurus.cimb.callmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CallMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
