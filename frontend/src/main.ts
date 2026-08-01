75dd7228bfc00a9daa3f50433ca32624302052f3
diego-saurus <aldiegoid20019@gmail.com>
2026-08-01 20:07:41 +0700

    feat(frontend): setup pinia colada as async state management

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,18 @@
 import './assets/css/main.css'
 import ui from '@nuxt/ui/vue-plugin'
+import { PiniaColada } from '@pinia/colada'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
 app.use(ui)
+app.use(pinia)
+app.use(PiniaColada)
 
 app.mount('#app')
