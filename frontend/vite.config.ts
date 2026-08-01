786fe812ca3fbb3cdb2219cad41bcd12a45fa015
diego-saurus <aldiegoid20019@gmail.com>
2026-08-02 01:20:55 +0700

    feat(frontend): update design system

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,24 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), nuxtUI()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    nuxtUI({
+      colorMode: false,
+      ui: {
+        colors: {
+          primary: 'raw-sienna',
+          secondary: 'cutty-sark',
+          destructive: 'coral-red',
+          success: 'vista-green',
+          info: 'powder-blue',
+          warning: 'candlelight',
+          neutral: 'marshland',
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
